89767fc609aba6277b6d9908345e30594e9a2878
Shahed Mehbub <shahed739@gmail.com>
2020-06-18 13:41:31 +0600

    update easy number 4

--- Easy/4.Number of steps to reduce a number to zero.py
+++ Easy/4.Number of steps to reduce a number to zero.py
@@ -1,10 +1,13 @@
 class Solution:
     def numberOfSteps (self, num: int) -> int:
-        counter = 0
-        while num != 0:
-            if num % 2 == 0:
-                num /= 2
-            else:
-                num -= 1
-            counter += 1
-        return counter
+        num = f'{num:b}'
+        return num.count('1') * 2 + num.count('0') - 1
+             
+# counter = 0
+# while num != 0:
+#     if num % 2 == 0:
+#         num /= 2
+#     else:
+#         num -= 1
+#     counter += 1
+# return counter
